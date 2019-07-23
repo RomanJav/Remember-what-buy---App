c83f6d7e59071b99f508cf26aa4e2fc398e38bf4
RomanJav <tomal.roman94@gmail.com>
2019-07-23 21:09:34 +0300

    Create main menu template(buttons and styles)

---
+++ src/main/java/by/tomal/rememberwhatbuy/menu/MainActivity.kt
@@ -0,0 +1,12 @@
+package by.tomal.rememberwhatbuy.menu
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import by.tomal.rememberwhatbuy.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
